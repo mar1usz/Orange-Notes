431183ab6e247d6fd48ef9889de59535580f12b4
Mariusz <mariopl@poczta.onet.eu>
2020-03-19 22:42:20 +0100

    pierwsza wersja modelu skończona

---
+++ Orange Notes/Model/Notes.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Orange_Notes.Model
+{
+    public class Notes
+    {
+        private List<Note> notes;
+
+        public Notes()
+        {
+            notes = new List<Note>();
+        }
+
+        public void AddNote()
+        {
+            if (!notes.Any())
+            {
+                notes.Add(new Note(1));
+            }
+            else
+            {
+                int i = notes.Max(n => n.noteId);
+                i++;
+
+                notes.Add(new Note(i));
+            }
+        }
+
+        public bool RemoveNote(int noteId)
+        {
+            int i = notes.FindIndex(n => n.noteId == noteId);
+
+            if (i != -1)
+                notes.RemoveAt(i);
+            else
+                return false;
+
+            return true;
+        }
+
+        public string GetNoteTitle(int noteId)
+        {
+            int i = notes.FindIndex(n => n.noteId == noteId);
+
+            if (i != -1)
+                return notes[i].noteTitle;
+            else
+                return null;
+        }
+
+        public string GetNoteContent(int noteId)
+        {
+            int i = notes.FindIndex(n => n.noteId == noteId);
+
+            if (i != -1)
+                return notes[i].noteContent;
+            else
+                return null;
+        }
+
+        public bool SetNoteTitle(int noteId, string noteTitle)
+        {
+            int i = notes.FindIndex(n => n.noteId == noteId);
+
+            if (i != -1)
+                notes[i].noteTitle = noteTitle;
+            else
+                return false;
+
+            return true;
+        }
+
+        public bool SetNoteContent(int noteId, string noteContent)
+        {
+            int i = notes.FindIndex(n => n.noteId == noteId);
+
+            if (i != -1)
+                notes[i].noteContent = noteContent;
+            else
+                return false;
+
+            return true;
+        }
+    }
+
+    public class Note
+    {
+        public int noteId { get; set; }
+        public string noteTitle { get; set; }
+        public string noteContent { get; set; }
+
+        public Note(int noteId)
+        {
+            this.noteId = noteId;
+            this.noteTitle = "Notka" + " " + "#" + noteId;
+        }
+    }
+}
--- Orange Notes/ViewModel/NoteViewModel.cs
+++ Orange Notes/ViewModel/NoteViewModel.cs
@@ -5,12 +5,6 @@ namespace Orange_Notes.ViewModel
 {
     public class NoteViewModel : ViewModelBase
     {
-        public ObservableCollection<Note> notes { get; set; }
-        public NoteViewModel()
-        {
-            notes = new ObservableCollection<Note>();
-            notes.Add(new Note("Uh, that's nice!"));
-            notes.Add(new Note("Now let me see you do the same thing twice"));
-        }
+
     }
 }
