5ee59af8c140baad5c14404916865a8e9d362b45
Mariusz <mariopl@poczta.onet.eu>
2020-03-19 18:57:21 +0100

    Add project files.

---
+++ Orange Notes/Model/Note.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orange_Notes.Model
+{
+    class Note
+    {
+        public string content { get; set; }
+    }
+}
